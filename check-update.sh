1a83bc3103c936edbc90e14e499ce674aeb7a5e5
mandian <mandian@users.noreply.github.com>
2022-12-16 17:43:04 +0000

    add check-update.sh

--- check-update.sh
+++ check-update.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-curl "https://github.com/BelledonneCommunications/bzrtp/tags" 2>/dev/null |grep "tag/" |sed -e 's,.*tag/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+#curl -s "https://github.com/BelledonneCommunications/bzrtp/tags" |grep "tag/" |sed -e 's,.*tag/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
+curl -s "https://gitlab.linphone.org/BC/public/bzrtp/-/tags" |grep "tags/" |sed -e 's,.*tags/,,;s,\".*,,;' |grep -E '^[0-9.]+$' |grep -v '[0-9][0-9][0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]' |sort -V |tail -n1
 
